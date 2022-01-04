e4821c222950f8a7f202d8411280ff0740ca1559
Camila Abreu <81877746+mila-developer@users.noreply.github.com>
2022-01-04 12:27:37 +0000

    ADD: modal window and event to button NO

--- completeGuide2022-CSS/module1/shared.js
+++ completeGuide2022-CSS/module1/shared.js
@@ -1,4 +1,31 @@
 var backdrop = document.querySelector('.backdrop');
+var modal= document.querySelector('.modal');
+var modalNoButton = document.querySelector('.modal__action--negative');
+var selectPlanButtons = document.querySelectorAll('.plan button');
 
-console.log(backdrop); //to see the element in the browser console
-console.dir(backdrop); // to see in a object
+// console.dir(selectPlanButtons);
+// console.log(backdrop); //to see the element in the browser console
+// console.dir(backdrop); // to see in a object
+
+
+// i = 0 -> the first element/button // i < selectPlanButtons -> the element
+// will run while as long i smoller than selectPlanButtons // length -> special proprerty to tell us how many elements we got
+
+// to show the modal window
+
+for(var i = 0; i < selectPlanButtons.length; i++) {
+  selectPlanButtons[i].addEventListener('click', function() {
+    modal.style.display = 'block';
+    modal.style.display = 'block';
+  });
+}
+
+// button NO to modal
+
+backdrop.addEventListener('click', closeModal);
+modal.addEventListener('click', closeModal);
+
+function closeModal() {
+  backdrop.style.display = 'none';
+  modal.style.display = 'none';
+}
