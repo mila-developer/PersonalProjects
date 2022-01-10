3c74836fd1b56b60e5636c5148548187333a29c9
Camila Abreu <81877746+mila-developer@users.noreply.github.com>
2022-01-10 11:49:04 +0000

    UPD: change the pallet of collors

--- completeGuide2022-CSS/module1/shared.js
+++ completeGuide2022-CSS/module1/shared.js
@@ -29,12 +29,17 @@ backdrop.addEventListener('click', function() {
   closeModal();
 });
 
-modal.addEventListener('click', closeModal);
+if (modalNoButton) {
+  modalNoButton.addEventListener('click', closeModal);
+}
+
 
 function closeModal() {
   // backdrop.style.display = 'none';
   // modal.style.display = 'none';
-  modal.classList.remove('open');
+  if (modal) {
+    modal.classList.remove('open');
+  }
   backdrop.classList.remove('open');
 }
 
