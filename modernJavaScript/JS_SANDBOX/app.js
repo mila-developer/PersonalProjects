96b30842311440ec8ca6b687c3240474b1b4f2e1
Camila Abreu <81877746+mila-developer@users.noreply.github.com>
2022-01-24 11:00:31 +0000

    UPD: string methods and concatenation

--- modernJavaScript/JS_SANDBOX/app.js
+++ modernJavaScript/JS_SANDBOX/app.js
@@ -1,3 +1,9 @@
+const firstName = 'William';
+const lastName = 'Johnson';
+const age = 36;
+const str = 'Hello there my name is Brad';
+const tags = 'web design, web development, programming';
+
 let val;
 
 // Number to string
@@ -59,5 +65,57 @@ val = Math.max(2,33,4,1,55,6,3,-2);
 val = Math.random();
 
 val = Math.floor(Math.random() * 20 + 1);
+
+console.log(val);
+
+// Concatenation
+val = firstName + '' + lastName;
+
+// Append
+val = 'Brad';
+val += 'Traversy';
+
+val = 'Hello, my name is' + firstName + 'and I am' + age;
+
+// Escaping
+val = 'That\'s awesome, I can\'t wait';
+
+// Length
+val = firstName.length;
+
+// concat
+val = firstName.concat(' ', lastName);
+
+// Change case
+val = firstName.toUpperCase();
+val = firstName.toLowerCase();
+
+val = firstName[2];
+
+// indexOf()
+val = firstName.indexOf('1');
+val = firstName.lastIndexOf('1');
+
+// charAt()
+val = firstName.chartAt('2');
+// Get last char
+val = firstName.chartAt(firstName.length - 1);
+
+// substring
+val = firstName.substring(0, 4);
+
+// slice()
+val = firstName.slice(0,4);
+val = firstName.slice(-3);
+
+// split()
+val = str.split(' ');
+val = tags.split(',');
+
+// replace()
+val = str.replace('Brad', 'Jack');
+
+// include
+val = str.includes('Hello');
 
 console.log(val);
