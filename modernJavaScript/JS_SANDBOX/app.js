32fd0c6a406a110b53e8f5f98069f5e3dafd3ba0
Camila Abreu <81877746+mila-developer@users.noreply.github.com>
2022-01-24 10:26:42 +0000

    ADD: operators js

---
+++ modernJavaScript/JS_SANDBOX/app.js
@@ -0,0 +1,63 @@
+let val;
+
+// Number to string
+val = String(555);
+val = String(4+4);
+// Bool to string
+val = String(true);
+// Date to string
+val = String(new Date());
+// Array to string
+val = String([1,2,3,4]);
+
+// toString()
+val = (5).toString();
+val = (true).toString();
+
+// String to number
+val = Number('5');
+val = Number(true);
+val = Number(false);
+val = Number(null);
+val = Number('hello');
+val = Number([1,2,3]);
+
+val = parseInt('100.30');
+val = parseFloat('100.30');
+
+// Output
+// console.log(val);
+// console.log(typeof val);
+// //console.log(val.length);
+// console.log(val.toFixed(2));
+
+const val1 = String(5);
+const val2 = 6;
+const sum = Number(val1 + val2);
+
+console.log(sum);
+console.log(typeof sum);
+
+//Simple math with numbers
+val = num1 + num2;
+val = num1 * num2;
+val = num1 - num2;
+val = num1 / num2;
+val = num1 % num2;
+
+// Math Object
+val = Math.PI;
+val = Math.E;
+val = Math.round(2.4);
+val = Math.ceil(2.4);
+val = Math.floor(2.8);
+val = Math.sqrt(64);
+val = Math.abs(-3);
+val = Math.pow(8, 2);
+val = Math.min(2,33,4,1,55,6,3,-2);
+val = Math.max(2,33,4,1,55,6,3,-2);
+val = Math.random();
+
+val = Math.floor(Math.random() * 20 + 1);
+
+console.log(val);
