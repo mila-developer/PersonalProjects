94e26c6e937c03a68719b515aee9fd84f38058b1
Camila Abreu <81877746+mila-developer@users.noreply.github.com>
2022-02-11 14:38:59 +0000

    UPD

--- modernJavaScript/JS_SANDBOX/app.js
+++ modernJavaScript/JS_SANDBOX/app.js
@@ -1,121 +1,20 @@
-const firstName = 'William';
-const lastName = 'Johnson';
-const age = 36;
-const str = 'Hello there my name is Brad';
-const tags = 'web design, web development, programming';
+const person = {
+  firstName: 'Steve',
+  lastName: 'Smith',
+  age: 30,
+  email: 'steve@aol.com',
+  hobbies: ['music', 'sports'],
+  address: {
+    city: 'Miami',
+    state
+  }
+}
 
 let val;
 
-// Number to string
-val = String(555);
-val = String(4+4);
-// Bool to string
-val = String(true);
-// Date to string
-val = String(new Date());
-// Array to string
-val = String([1,2,3,4]);
-
-// toString()
-val = (5).toString();
-val = (true).toString();
-
-// String to number
-val = Number('5');
-val = Number(true);
-val = Number(false);
-val = Number(null);
-val = Number('hello');
-val = Number([1,2,3]);
-
-val = parseInt('100.30');
-val = parseFloat('100.30');
-
-// Output
-// console.log(val);
-// console.log(typeof val);
-// //console.log(val.length);
-// console.log(val.toFixed(2));
-
-const val1 = String(5);
-const val2 = 6;
-const sum = Number(val1 + val2);
-
-console.log(sum);
-console.log(typeof sum);
-
-//Simple math with numbers
-val = num1 + num2;
-val = num1 * num2;
-val = num1 - num2;
-val = num1 / num2;
-val = num1 % num2;
-
-// Math Object
-val = Math.PI;
-val = Math.E;
-val = Math.round(2.4);
-val = Math.ceil(2.4);
-val = Math.floor(2.8);
-val = Math.sqrt(64);
-val = Math.abs(-3);
-val = Math.pow(8, 2);
-val = Math.min(2,33,4,1,55,6,3,-2);
-val = Math.max(2,33,4,1,55,6,3,-2);
-val = Math.random();
-
-val = Math.floor(Math.random() * 20 + 1);
-
-console.log(val);
-
-// Concatenation
-val = firstName + '' + lastName;
-
-// Append
-val = 'Brad';
-val += 'Traversy';
-
-val = 'Hello, my name is' + firstName + 'and I am' + age;
-
-// Escaping
-val = 'That\'s awesome, I can\'t wait';
-
-// Length
-val = firstName.length;
-
-// concat
-val = firstName.concat(' ', lastName);
-
-// Change case
-val = firstName.toUpperCase();
-val = firstName.toLowerCase();
-
-val = firstName[2];
-
-// indexOf()
-val = firstName.indexOf('1');
-val = firstName.lastIndexOf('1');
-
-// charAt()
-val = firstName.chartAt('2');
-// Get last char
-val = firstName.chartAt(firstName.length - 1);
-
-// substring
-val = firstName.substring(0, 4);
-
-// slice()
-val = firstName.slice(0,4);
-val = firstName.slice(-3);
-
-// split()
-val = str.split(' ');
-val = tags.split(',');
-
-// replace()
-val = str.replace('Brad', 'Jack');
-
-// include
-val = str.includes('Hello');
+val = person;
+//Get specific value
+val = person.firstName;
+val = person['lastName'];
 
 console.log(val);
